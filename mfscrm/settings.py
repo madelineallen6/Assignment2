9e3a6e92902aa2cb7411d5103e55fdee5d6c0239
madelineallen6 <59840216+madelineallen6@users.noreply.github.com>
2020-02-28 22:46:38 -0600

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -121,11 +121,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
 
